97aea7e546c4e83b25e9ab382ad1d404f4f16412
Paulo-Dandrea <prog.dandrea@gmail.com>
2021-02-24 10:14:42 -0300

    cards

--- front-end/src/Components/ChatMessage.jsx
+++ front-end/src/Components/ChatMessage.jsx
@@ -9,12 +9,14 @@ const containerStyle = {
 const cardStyle = {
   display: 'flex',
   width: '90%',
-  alignItems: 'center',
+  flexDirection: 'column',
+  // alignItems: 'center',
+  // justifyContent: 'space-around',
 };
 
 const cardPhotoStyle = {
   width: '20%',
-  margin: '0 12px',
+  // margin: '0 12px',
 };
 
 const cardMessageStyle = {
@@ -31,7 +33,7 @@ const getTime = (date) => {
   const hh = date.getHours();
   const mm = date.getMinutes();
   return `${hh}:${mm >= 10 ? '' : '0'}${mm}`;
-}
+};
 
 const ChatMessage = ({ buffer, isSelf }) => {
   const { from = store, message, createdAt } = buffer;
@@ -41,31 +43,39 @@ const ChatMessage = ({ buffer, isSelf }) => {
   return (
     <div style={{ ...containerStyle, flexDirection }}>
       <div
-        className={`card-panel  ${isSelf ? 'blue-mid-bg' : 'blue-dark-bg' } lighten-3 z-depth-1`}
-        style={{ ...cardStyle, flexDirection }}
+        className={`card-panel space-around   ${
+          isSelf ? 'blue-mid-bg' : 'blue-dark-bg'
+        } lighten-3 z-depth-1`}
+        style={{ ...cardStyle }}
       >
-        <div style={cardPhotoStyle}>
-          <img
-            src={`https://ui-avatars.com/api/?name=${from?.email}&size=48`}
-            alt=""
-            className="circle responsive-img"
-          />
-        </div>
-        <div style={{ ...cardMessageStyle, textAlign: messageAlign }}>
-          <span className="white-mid-cl">
-            <small data-testid="nickname">{from?.email}</small> - {' '}
-            <small data-testid="message-time">{getTime(date)}</small>
-          </span>
-          <hr />
-          <span
-            data-testid="text-message"
-            className="white-mid-cl"
-          >
-            { message }
+        <div>
+          <span data-testid="text-message" className="white-mid-cl">
+            {message}
           </span>
         </div>
+
+        <div className="space-between">
+
+          <div style={{ ...cardMessageStyle, textAlign: messageAlign }}>
+            <span className="white-mid-cl">
+              <small data-testid="nickname">{from?.email}</small> -{' '}
+              <small data-testid="message-time">{getTime(date)}</small>
+            </span>
+          </div>
+
+          <div>
+            <img
+              src={`https://ui-avatars.com/api/?name=${from?.email}&size=32`}
+              alt=""
+              className="circle responsive-img"
+            />
+          </div>
+
+
+        </div>
+
       </div>
-    </div>  
+    </div>
   );
 };
 
